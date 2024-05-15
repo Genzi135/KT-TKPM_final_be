4ed6688b935ab67eecbd72a41cf596237e45922b
hungtang <clemencedoan128@gmail.com>
2024-05-15 08:09:28 +0700

    Add teacher name to enrollment response

--- src/enrollment/dtos/EnrollmentResponse.dto.ts
+++ src/enrollment/dtos/EnrollmentResponse.dto.ts
@@ -1,17 +1,19 @@
-import { Enrollment } from "src/entites/Enrollment.entity";
+import { Enrollment } from 'src/entites/Enrollment.entity';
 
 export class EnrollmentResponse {
-    id: number;
-    className: string;
-    currentStudents: number;
-    maxStudents: number;
-    isEnrolled: boolean;
+  id: number;
+  className: string;
+  currentStudents: number;
+  maxStudents: number;
+  teacherName: string;
+  isEnrolled: boolean;
 
-    constructor(enrollment: Enrollment){
-        this.id = enrollment.id;
-        this.className = enrollment.class.name;
-        this.currentStudents = enrollment.class.currentStudents;
-        this.maxStudents = enrollment.class.maxStudents;
-        this.isEnrolled = enrollment.isEnrolled;
-    }
+  constructor(enrollment: Enrollment) {
+    this.id = enrollment.id;
+    this.className = enrollment.class.name;
+    this.teacherName = enrollment.class.teacher.name;
+    this.currentStudents = enrollment.class.currentStudents;
+    this.maxStudents = enrollment.class.maxStudents;
+    this.isEnrolled = enrollment.isEnrolled;
+  }
 }
